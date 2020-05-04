b36aa9c824b209aeebc4b4b61fa23f9d03252fed
EvaNaabye <evan19@ru.is>
2020-05-04 23:27:19 +0000

    SADness

--- captain_console/games/views.py
+++ captain_console/games/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+game = [
+    {'name': 'Super Mario Bros 2',
+     'price': 4000,
+     'description': 'Mario is back and better than ever in a new Super Mario Bros 2 full of exiting new levels.',
+     'genre': 'action, adventure',
+     'publisher': 'Nintendo',
+     'releaseyear': '1988',
+     'platform': 'NES, Wii, Nintendo 3DS...'
+     },
+    {'name': 'The Legend of Zelda: Ocarina of Time',
+     'price': 4000,
+     'description': 'Link, a young warrior sets off on an adventure of a lifetime',
+     'genre': 'action, adventure',
+     'publisher': 'Nintendo',
+     'releaseyear': 1998,
+     'platform': 'Nintendo 64, Wii, Nintendo DS...'
+     }
+]
+
 def index(request):
-    return render(request, "games/index.html")
+    return render(request, 'games/index.html', context={
+        'game': game
+    })
