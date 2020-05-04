ed653af6c81478c68395880913174b9ccd413c37
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-04 15:59:58 +0000

    venv removed

--- captain_console/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/bjorgvinelisson/Desktop/HRyear1/verklegt2/verklegt2/captain_console/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
