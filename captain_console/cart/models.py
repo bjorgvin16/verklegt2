454c0962c99d0ce9be8d1b5938309860314460d9
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-05 21:11:33 +0000

    ups

--- captain_console/cart/models.py
+++ captain_console/cart/models.py
@@ -4,7 +4,7 @@ from frontpage.models import Product
 
 class Cart(models.Model):
     customerId = models.ForeignKey(User, on_delete=models.CASCADE)
-    inuse = models.BooleanField()
+    active = models.BooleanField()
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
