2c2eb391d7f90cb3154e0038a73758262ae5179b
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-05 18:50:20 +0000

    :clock9:

--- captain_console/cart/models.py
+++ captain_console/cart/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from userprofile.models import User
-# it's not perfect but it's something
+from frontpage.models import Product
 
 class Cart(models.Model):
-    totalPrice = models.IntegerField()
-    itemNum = models.IntegerField()
     customerId = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
--- captain_console/userprofile/models.py
+++ captain_console/userprofile/models.py
@@ -3,15 +3,16 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=255)
 
-class Address(models.Model):
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
     streetname = models.CharField(max_length=255)
     housenumber = models.IntegerField()
     zipcode = models.IntegerField()
     city = models.CharField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
-class User(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+class phoneNr(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phoneNr = models.IntegerField()
