6c59eeeeaa8782501d1d1964c7d933ad8dbe515d
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-05 13:27:30 +0000

    game genre tble added

--- captain_console/games/models.py
+++ captain_console/games/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Game(models.Model):
     name = models.Charfield(max_length=255)
     description = models.CharField(max_length=999)
+
+class GameGenre(models.Model):
+    name = models.CharField(max_length=255)
