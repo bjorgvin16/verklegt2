e638e34cf743a608a0e219b6c59f612d566b4b12
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-05 21:25:02 +0000

    pushpush baby

--- captain_console/userprofile/models.py
+++ captain_console/userprofile/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
 
 class phoneNr(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     phoneNr = models.IntegerField()
+
+class UserImage(models.Model):
+    image = models.CharField(max_length=999)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
