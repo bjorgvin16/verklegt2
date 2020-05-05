4a56711d4a88bd1ac0b5190d6596e838fa72b13e
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-05 21:53:05 +0000

    database??

--- captain_console/userprofile/models.py
+++ captain_console/userprofile/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
 
 class phoneNr(models.Model):
