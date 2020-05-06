e89da061ac888c0d1becab98a95ccbe6d3c720c5
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-06 14:02:03 +0000

    mimimi

--- captain_console/consoles/views.py
+++ captain_console/consoles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from consoles.models import Console
 
 def index(request):
+    context = {"consoles": Console.objects.all().order_by("name")}
     return render(request, "consoles/index.html")
