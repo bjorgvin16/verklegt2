e8c81132a0bf0401cd2fc5e8c4edcdda38f6299d
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-06 21:14:53 +0000

    myndir laga

--- captain_console/accessories/urls.py
+++ captain_console/accessories/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     # mun vísa á http://localhost:8000/accessories
     path('', views.index, name="accessories-index"),
+    path('<int:id>', views.get_console_by_id, name="accessories-details"),
 ]
--- captain_console/accessories/views.py
+++ captain_console/accessories/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from accessories.models import Accessory
-
 
 def index(request):
     context = { "accessories": Accessory.objects.all().order_by("name") }
     return render(request, "accessories/index.html", context)
+
+def get_console_by_id(request, id):
+    context = {"accessory": get_object_or_404(Accessory, pk=id)}
+    return render(request, "accessories/accessory_details.html", context)
