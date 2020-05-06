613629d512fd69575af9e7fce2594803ac3e2b4a
m4gga <margriette123@gmail.com>
2020-05-06 13:52:21 +0000

    deleted console dummy data branch master

--- captain_console/consoles/views.py
+++ captain_console/consoles/views.py
@@ -1,24 +1,4 @@
 from django.shortcuts import render
 
-consoles = [
-    {'name': 'Super Mario Bros 2',
-     'price': 4000,
-     'description': 'Mario is back and better than ever in a new Super Mario Bros 2 full of exiting new levels.',
-     'genre': 'action, adventure',
-     'publisher': 'Nintendo',
-     'releaseyear': '1988',
-     'platform': 'NES, Wii, Nintendo 3DS...'
-     },
-    {'name': 'The Legend of Zelda: Ocarina of Time',
-     'price': 4000,
-     'description': 'Link, a young warrior sets off on an adventure of a lifetime',
-     'genre': 'action, adventure',
-     'publisher': 'Nintendo',
-     'releaseyear': 1998,
-     'platform': 'Nintendo 64, Wii, Nintendo DS...'
-     }
-]
-
-
 def index(request):
     return render(request, "consoles/index.html")
