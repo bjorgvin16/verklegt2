ef07e47f80bcc75977124001e1ce26bd1c06e94b
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-06 15:08:38 +0000

    consoles index

--- captain_console/consoles/views.py
+++ captain_console/consoles/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from consoles.models import Console
 
 def index(request):
-    return render(request, "consoles/index.html", context={"consoles": Console.objects.all()})
+    context = { "consoles": Console.objects.all().order_by("name") }
+    return render(request, "consoles/index.html", context)
--- captain_console/games/views.py
+++ captain_console/games/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from games.models import Game
 
 def index(request):
-    context = { 'games': Game.objects.all().order_by('name') }
-    return render(request, 'games/index.html', context)
+    context = { "games": Game.objects.all().order_by("name") }
+    return render(request, "games/index.html", context)
