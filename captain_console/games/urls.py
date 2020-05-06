f2b46eae9f530c9282d63d37e0bf89549327c053
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-06 21:00:47 +0000

    wowowow

--- captain_console/games/urls.py
+++ captain_console/games/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     # http://localhost:8000/games
     path('', views.index, name="games-index"),
-
+    path('<int:id>', views.get_game_by_id, name="games-details"),
 ]
--- captain_console/games/views.py
+++ captain_console/games/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from games.models import Game
 
 def index(request):
     context = { "games": Game.objects.all().order_by("name") }
     return render(request, "games/index.html", context)
+
+def get_game_by_id(request, id):
+    context = {"game": get_object_or_404(Game, pk=id)}
+    return render(request, "games/game_details.html", context)
