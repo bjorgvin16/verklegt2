128771eb07cc8c3b09afdaadfb2ad5bd28ab1627
m4gga <margriette123@gmail.com>
2020-05-07 14:55:04 +0000

    sign up virkar loksins

--- captain_console/signup/views.py
+++ captain_console/signup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from .forms import CreateUserForm
 
 def index(request):
@@ -13,12 +14,7 @@ def index(request):
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + user)
 
-            return redirect('login')
-
-
             return redirect('login-index')
-
-
 
     context = {'form' : form}
     return render(request, 'signup/index.html', context)
