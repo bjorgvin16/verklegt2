5694c4b6b7dbd4fc4f2f71ab100e9f26e60479f1
EvaNaabye <evan19@ru.is>
2020-05-08 18:38:49 +0000

    alls staðar filters

--- captain_console/accessories/urls.py
+++ captain_console/accessories/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     # mun vísa á http://localhost:8000/accessories
     path('', views.index, name="accessories-index"),
     path('<int:id>', views.get_console_by_id, name="accessories-details"),
+    path('desc/', views.order_by_desc, name="filter_desc"),
+    path('lowhigh/', views.order_by_lowest_highest, name="lowest_highest"),
+    path('highlow/', views.order_by_highest_lowest, name="highest_lowest")
 ]
--- captain_console/accessories/views.py
+++ captain_console/accessories/views.py
@@ -8,3 +8,15 @@ def index(request):
 def get_console_by_id(request, id):
     context = {"accessory": get_object_or_404(Accessory, pk=id)}
     return render(request, "accessories/accessory_details.html", context)
+
+def order_by_desc(request):
+    context = { "accessories": Accessory.objects.all().order_by("-name") }
+    return render(request, "accessories/index.html", context)
+
+def order_by_highest_lowest(request):
+    context = {"accessories": Accessory.objects.all().order_by("-price")}
+    return render(request, "accessories/index.html", context)
+
+def order_by_lowest_highest(request):
+    context = {"accessories": Accessory.objects.all().order_by("price")}
+    return render(request, "accessories/index.html", context)
