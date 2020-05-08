f15001946922c687dc0bcb8d5902f153f496c84b
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-08 16:59:43 +0000

    aaaa pycharm er að krassa, search er mjööööög nálægt þvi að vera komið

--- captain_console/productsearch/views.py
+++ captain_console/productsearch/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from frontpage.models import Product
+from games.models import Game
+from accessories.models import Accessory
+from consoles.models import Console
 from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
+    '''
     if 'search' in request.GET:
         search = request.GET['search']
-        products = [{
+        context = [{
             'id': x.id,
             'name': x.name,
             'description': x.description,
             'firstImage': x.productimage_set.first().image
         } for x in Product.objects.filter(name__icontains=search) ]
         return JsonResponse({ 'data': products })
-    return render(request, 'productsearch/index.html')
+
+    if 'search' in request.GET:
+        '''
+    search = request.GET['search']
+    context = {
+        "products": Product.objects.filter(name__icontains=search) ,
+        "games": Game.objects.all(),
+        "accessories": Accessory.objects.all(),
+        "consoles": Console.objects.all(),
+    }
+    return render(request, 'productsearch/index.html', context)
