87612e9757c2e29409ed02dc1f88a603c8aba1db
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-08 11:32:36 +0000

    search functionality beta

--- captain_console/frontpage/views.py
+++ captain_console/frontpage/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from frontpage.models import Product
+from django.http import JsonResponse
 
 def index(request):
+    if 'search' in request.GET:
+        search = request.GET['search']
+        products = [{
+            'id': x.id,
+            'name': x.name,
+            'description': x.description,
+            'firstImage': x.productimage_set.first().image
+        } for x in Product.objects.filter(name__icontains=search) ]
+        return JsonResponse({ 'data': products })
     return render(request, 'frontpage/index.html')
