e0ff74bbdb783a56ea38ac1814be67373b7ca5dd
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-08 19:10:40 +0000

    osom

--- captain_console/consoles/urls.py
+++ captain_console/consoles/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # mun vísa á http://localhost:8000/consoles
     path('', views.index, name="consoles-index"),
     path('<int:id>', views.get_console_by_id, name="consoles-details"),
-    path('desc/', views.order_by_desc, name="filter_desc"),
-    path('lowhigh/', views.order_by_lowest_highest, name="lowest_highest"),
-    path('highlow/', views.order_by_highest_lowest, name="highest_lowest")
+    path('desc/', views.order_by_desc, name="consoles-filter_desc"),
+    path('lowhigh/', views.order_by_lowest_highest, name="consoles-lowest_highest"),
+    path('highlow/', views.order_by_highest_lowest, name="consoles-highest_lowest")
 ]
--- captain_console/games/urls.py
+++ captain_console/games/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     # http://localhost:8000/games
     path('', views.index, name="games-index"),
     path('<int:id>', views.get_game_by_id, name="games-details"),
-    path('desc/', views.order_by_desc, name="filter_desc"),
-    path('lowhigh/', views.order_by_lowest_highest, name="lowest_highest"),
-    path('highlow/', views.order_by_highest_lowest, name="highest_lowest")
+    path('desc/', views.order_by_desc, name="games-filter_desc"),
+    path('lowhigh/', views.order_by_lowest_highest, name="games-lowest_highest"),
+    path('highlow/', views.order_by_highest_lowest, name="games-highest_lowest")
 
 ]
