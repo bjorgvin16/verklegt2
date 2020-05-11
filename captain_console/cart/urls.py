f4c3043a1f967cee686f7af8cc73e05e3d52f17b
m4gga <margriette123@gmail.com>
2020-05-11 18:31:00 +0000

    a bunch of cart stuff

--- captain_console/cart/urls.py
+++ captain_console/cart/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
 from . import views
+from .views import (
+    add_to_cart,
+    delete_from_cart,
+    order_details,
+    checkout,
+    process_payment,
+    update_transaction_records,
+    success
+)
+app_name = 'cart'
+
 urlpatterns = [
-    # mun vísa á http://localhost:8000/carts
-    path('', views.index, name="cart-index"),
+    url(r'^add-to-cart/(?P<item_id>[-\w]+)/$', add_to_cart, name="add_to_cart"),
+    url(r'^order-summary/$', order_details, name="order_summary"),
+    url(r'^success/$', success, name='purchase_success'),
+    url(r'^item/delete/(?P<item_id>[-\w]+)/$', delete_from_cart, name='delete_item'),
+    url(r'^checkout/$', checkout, name='checkout'),
+    url(r'^update-transaction/(?P<token>[-\w]+)/$', update_transaction_records,
+        name='update_records')
 ]
--- captain_console/cart/views.py
+++ captain_console/cart/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django import render, redirect
+from django.contrib.auth import login_required
 
-def index(request):
-    return render(request, 'cart/index.html', context={
-    })
+
+def get_user_pending_order():
+    ''' getting the order that a user has and hasn't payed for'''
+    pass
+
+@login_required
+def delete_item(request, item_id):
+    '''deletes the item with the item id in the cart'''
+    pass
+
+@login_required
+def add_item_to_cart(request, item_id):
+    '''adds item with item_id to cart'''
+
+@login_required
+def process_payment():
+    '''function for checkout basically'''
+    pass
+
+@login_required()
+def get_order_details(request, **kwargs):
+    ''' detailed information about the order'''
+    existing_order = get_user_pending_order(request)
+    context = {
+        'order': existing_order
+    }
+    return render(request, 'shopping_cart/order_summary.html', context)
+
+@login_required
+def checkout():
+    '''let's go boys'''
+    pass
