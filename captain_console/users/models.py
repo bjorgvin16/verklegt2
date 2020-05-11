b3c59f84459eab1f861ec7142d10018d73fe688d
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-11 13:22:32 +0000

    help

--- captain_console/users/models.py
+++ captain_console/users/models.py
@@ -1,17 +1,16 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from frontpage.models import Product
-
-User = get_user_model()
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     products = models.ManyToManyField(Product, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username} Profile'
 
 def post_save_profile_create(sender, instance, created, *args, **kwargs):
     if created:
