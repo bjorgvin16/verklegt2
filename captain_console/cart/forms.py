eee36070ef1ffb965e39661001d9b970b9093e5d
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-12 17:23:58 +0000

    help

--- captain_console/cart/forms.py
+++ captain_console/cart/forms.py
@@ -1,4 +1,17 @@
 from django import forms
+from django_countries.fields import CountryField
+from . import validators
 
-class ContactInfoForm(forms.ModelForm):
-    contact = 1
+class ContactInfoForm(forms.Form):
+    contact = forms.CharField(max_length=255)
+    street_name = forms.CharField(max_length=255)
+    house_number = forms.IntegerField()
+    city = forms.CharField(max_length=255)
+    zip = forms.IntegerField()
+    country = CountryField()
+
+class PaymentInfoForm(forms.Form):
+    cardholder = forms.CharField(max_length=255)
+    card_number = forms.CharField(validators=[validators.card_num_validator])
+    exp_date = forms.CharField(max_length=5, validators=[validators.exp_date_validator])
+    cvc_code = forms.IntegerField(min_value=100, max_value=999)
