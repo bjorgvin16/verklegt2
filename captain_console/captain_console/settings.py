debaca8d5b446c8021ff2bf3b45831957709d6a6
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-12 12:47:40 +0000

    oklagaehvstatic

--- captain_console/captain_console/settings.py
+++ captain_console/captain_console/settings.py
@@ -132,11 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
