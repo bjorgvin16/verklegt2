4ff69ecfe76930cb54224277670427293c50b0db
m4gga <margriette123@gmail.com>
2020-05-12 17:10:29 +0000

    cart einfalt

--- captain_console/cart/models.py
+++ captain_console/cart/models.py
@@ -1,37 +1,20 @@
-from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 from users.models import Profile
 from frontpage.models import Product
 
 
-class OrderItem(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.SET_NULL, null=True)
-    is_ordered = models.BooleanField(default=False)
-    date_added = models.DateTimeField(auto_now=True)
-    date_ordered = models.DateTimeField(null=True)
-    quantity = models.IntegerField(default=1)
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
-    def __str__(self):
-        return self.product.name
+    #def get_cart_items(self):
+    #    return self.items.all()
 
+    #def get_cart_total(self):
+    #    return sum([item.product.price for item in self.items.all()])
 
-class Order(models.Model):
-    ref_code = models.CharField(max_length=15)
-    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    is_ordered = models.BooleanField(default=False)
-    items = models.ManyToManyField(OrderItem)
-    # payment_details = models.ForeignKey(Payment, null=True)
-    date_ordered = models.DateTimeField(auto_now=True)
-
-    def get_cart_items(self):
-        return self.items.all()
-
-    def get_cart_total(self):
-        return sum([item.product.price for item in self.items.all()])
-
-    def __str__(self):
-        return '{{self.owner}}  {{self.items}}'
 
 
--- captain_console/cart/urls.py
+++ captain_console/cart/urls.py
@@ -3,27 +3,18 @@ from . import views
 from .views import (
     add_item_to_cart,
     delete_cart_item,
-    get_order_details,
+    get_cart_items,
     checkout,
     process_payment,
-    update_order_records,
     success
 )
 
 
 urlpatterns = [
-    path('', views.get_user_pending_order, name="cart-index"),
-    path('add-to-cart/<int:id>/', views.add_item_to_cart, name="cart-add_item_to_cart"),
-    path('get-order-details/', views.get_order_details, name="cart-get_order_details" ),
+    path('', views.get_cart_items, name="cart-index"),
+    path('add-to-cart/<int:product_id>/', views.add_item_to_cart, name="cart-add_item_to_cart"), #    path('<int:id>', views.get_game_by_id, name="games-details"),
+    path('get-cart-items/', views.get_cart_items, name="cart-get_cart_details" ),
     path('checkout/', views.checkout, name="cart-checkout"),
     path('success/', views.success, name="cart-success"),
     path('delete-item/<int:id>/', views.delete_cart_item, name="cart-delete_item")
-
-    #path(r'^add-to-cart/(?P<item_id>[-\w]+)/$', add_item_to_cart, name="add_to_cart"),
-    #path(r'^order-summary/$', get_order_details, name="order_summary"),
-    #path(r'^success/$', success, name='purchase_success'),
-    #path(r'^item/delete/(?P<item_id>[-\w]+)/$', delete_cart_item, name='delete_item'),
-    #path(r'^checkout/$', checkout, name='checkout'),
-    #path(r'^update-transaction/(?P<token>[-\w]+)/$', update_transaction_records,
-    #    name='update_records')
 ]
