4415ba990ca9c6b3d78983ceb89a3925673b6639
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-12 21:52:11 +0000

    ok

--- captain_console/games/urls.py
+++ captain_console/games/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<int:id>', views.get_game_by_id, name="games-details"),
     path('desc/', views.order_by_desc, name="games-filter_desc"),
     path('lowhigh/', views.order_by_lowest_highest, name="games-lowest_highest"),
-    path('highlow/', views.order_by_highest_lowest, name="games-highest_lowest")
-
+    path('highlow/', views.order_by_highest_lowest, name="games-highest_lowest"),
+    path('manufacturers/<int:manufacturerid>/', views.get_manufacturer_by_id, name="games-manufacturer")
 ]
