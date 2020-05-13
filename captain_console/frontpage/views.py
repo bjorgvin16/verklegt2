6dc4df215a34e349dfafd66b5b6f8908fae1f735
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-13 18:29:31 +0000

    mood

--- captain_console/frontpage/views.py
+++ captain_console/frontpage/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 #from cart import Order
 from users.models import ProductView
-from .models import Product, Manufacturer
 from helpers.views import buildContext
 from consoles.models import Console
 from games.models import Game
@@ -13,19 +12,3 @@ def index(request):
     context["consoles"] = Console.objects.all().order_by("-created_date")
     context["game"] = Game.objects.all()
     return render(request, 'frontpage/index.html', context)
-
-def product_list(request):
-    object_list = Product.objects.all()
-    filtered_orders = Order.objects.filter(owner=request.user.profile, is_ordered = True)
-    current_order_products = []
-    if filtered_orders.exists():
-        user_order = filtered_orders[0]
-        user_order_items = user_order.items.all()
-        current_order_products = [product.product for product in user_order_items]
-
-        context = {
-            'object_list': object_list,
-            'current_order_products': current_order_products
-        }
-
-        return render(request, 'products/product_list.html', context) # need to redirect this
