fc45e3522c48a521734649b56176804df5d9cd5d
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-13 18:03:57 +0000

    síðan runnar en checkout hja mer er i eh rugli i am want to die

--- captain_console/checkout/urls.py
+++ captain_console/checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/contact', views.checkout, name="checkout-contact"),
+    path('contact', views.checkout, name="checkout-contact"),
+    path('payment', views.payment, name="checkout-payment"),
 ]
