c0a9d3442fd90b4bbe1b694c136c0344151924a7
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2020-05-13 19:49:39 +0000

    yaaaaaaaaas tengingar virka núna yas queen slay the boots haus down

--- captain_console/static/js/checkout.js
+++ captain_console/static/js/checkout.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
     $('#contact-next').on('click', function(e) {
         e.preventDefault();
         $.ajax({
-            url: 'checkout-payment',
+            url: 'checkout/payment',
             type: 'POST',
             success: function(resp) {
 
             },
             error: function (xhr, status, error) {
+                //TODO:this? idfk
                 console.error(error);
             }
         })
