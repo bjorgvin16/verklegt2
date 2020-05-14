a60cd13138cf02921ae9780f6eb21ab7fa2856e0
m4gga <margriette123@gmail.com>
2020-05-14 12:36:12 +0000

    order views (welcome to new york, we've been waiting for you)

--- captain_console/cart/views.py
+++ captain_console/cart/views.py
@@ -8,6 +8,18 @@ from checkout.forms import ContactInfoForm, PaymentInfoForm
 from django_countries import Countries
 import datetime
 
+
+
+#############       CART FUNCTIONS
+
+@login_required
+def clear_user_cart_data(request):
+    data_to_delete = Cart.objects.filter(user=request.user)
+    for data in data_to_delete:
+        data.delete()
+
+    return render(request, 'frontpage/index.html')
+
 @login_required
 def delete_cart_item(request, cart_id):
     '''deletes the item with the item id in the cart'''
@@ -25,15 +37,18 @@ def add_item_to_cart(request, product_id):
     return render(request, 'frontpage/index.html')
 
 
-@login_required
-def clear_user_cart_data(request):
-    data_to_delete = Cart.objects.filter(user=request.user)
-    for data in data_to_delete:
-        data.delete()
+@login_required()
+def get_cart_items(request):
+    carts = Cart.objects.filter(user=request.user)
+    if carts.exists():
+        #get all the items for this cart
+        context = {"carts": carts}
+        return render(request, 'cart/index.html', context)
+    else:
+        return render(request, 'cart/empty.html')
 
 
-    return render(request, 'frontpage/index.html')
-
+############           ORDER FUNCTIONS
 
 @login_required
 def get_products_for_order(request):
@@ -57,19 +72,21 @@ def create_order(request):
     return render(request, 'checkout/checkout.html')
 
 
-@login_required()
-def get_cart_items(request):
-    carts = Cart.objects.filter(user=request.user)
-    if carts.exists():
-        #get all the items for this cart
-        context = {"carts": carts}
-        return render(request, 'cart/index.html', context)
-    else:
-        return render(request, 'cart/empty.html')
+@login_required
+def get_order_items(request):
+    orders = Order.objects.filter(user=request.user)
+    if orders.exists():
+        #get all the items for this order for review
+        context = {"orders": carts}
+        return render(request, 'checkout/review.html', context)
 
 @login_required
-def success(request):
-    print('wow, so much success')
-    print('this is so amazing')
-    return render(request, 'cart/index.html')
+def get_total_order_price(request, order_id):
+    total_sum = 0
+    product_list = get_products_for_order()
+    for product in product_list:
+        total_sum += product.price
 
+    print(total_sum)
+
+    return total_sum
