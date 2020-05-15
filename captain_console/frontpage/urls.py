28a4825d7fcdb6cf429b4a422851686d787fa72b
EvaNaabye <evan19@ru.is>
2020-05-15 01:16:27 +0000

    filtered all products and stuff

--- captain_console/frontpage/urls.py
+++ captain_console/frontpage/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name="frontpage-index"),
     path('search', views.index, name="search-index"),
     path('get_all_products/', views.get_all_products, name="all-products"),
+    path('desc/', views.order_by_desc, name="products-filter_desc"),
+    path('lowhigh/', views.order_by_lowest_highest, name="products-lowest_highest"),
+    path('highlow/', views.order_by_highest_lowest, name="products-highest_lowest"),
 ]
