d8d989aef0ce3731dcbefec51d40dc69311f2654
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-15 12:38:15 +0000

    mimi

--- captain_console/helpers/views.py
+++ captain_console/helpers/views.py
@@ -1,4 +1,27 @@
+from accessories.models import Accessory
+from consoles.models import Console
 from frontpage.models import Manufacturer
+from games.models import Game
+
 
 def buildContext():
     return {"manufacturers": Manufacturer.objects.all().order_by("name")}
+
+def findTypeFromId(productid):
+    games = Game.objects.all()
+
+    for game in games:
+        if game.id == productid:
+            return "game"
+
+    accessories = Accessory.objects.all()
+
+    for accessory in accessories:
+        if accessory.id == productid:
+            return "accessory"
+
+    consoles = Console.objects.all()
+
+    for console in consoles:
+        if console.id == productid:
+            return "console"
