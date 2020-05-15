4283b8fc35f59c7991c60472fb6451aaafa6538c
svana00 <54863023+svana00@users.noreply.github.com>
2020-05-15 22:03:05 +0000

    :))))))))) undirhaka

--- captain_console/productsearch/views.py
+++ captain_console/productsearch/views.py
@@ -9,11 +9,12 @@ from helpers.views import buildContext
 def index(request):
     context = buildContext()
 
-    search = request.GET['search']
-    context["products"] = Product.objects.filter(name__icontains=search)
-    context["games"] = Game.objects.all()
-    context["accessories"] = Accessory.objects.all()
-    context["consoles"] = Console.objects.all()
-    context["search"] = search
+    search = request.GET.get('search', None)
+    if search:
+        context["products"] = Product.objects.filter(name__icontains=search)
+        context["games"] = Game.objects.all()
+        context["accessories"] = Accessory.objects.all()
+        context["consoles"] = Console.objects.all()
+        context["search"] = search
 
     return render(request, 'productsearch/index.html', context)
